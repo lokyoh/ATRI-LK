f3d1e878b69888b39bdaea56cfc069fea15a8707
l_o_o_k <2366360924@qq.com>
2025-09-25 06:26:34 +0800

    YHN-LK0-017 Release: 新的`运势`与`图库`插件, `聊天`新增AI聊天表情发送, 为`ATRI`服务新增白名单, 新添`用户`排行功能,修改事件工具, `钓鱼`成就注册方法参数的修改。

--- ATRI/__init__.py
+++ ATRI/__init__.py
@@ -6,7 +6,7 @@ from .dir import *
 
 __version__ = "YHN-LK0-017"
 """版本号"""
-__sub_version__ = "Pre2"
+__sub_version__ = "Release"
 """次版本号"""
 __conf_path = Path(".") / "config.yml"
 __conf = Config(__conf_path)
